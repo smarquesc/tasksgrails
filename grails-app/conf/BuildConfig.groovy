f27c2ff7373c2d28dc6089010d48904ec112b68b
Sandro Marques <sandro.marques.comp@gmail.com>
2015-08-09 23:25:24 -0300

    Testes configuração

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -73,5 +73,8 @@ grails.project.dependency.resolution = {
         //compile ":less-asset-pipeline:1.10.0"
         //compile ":coffee-asset-pipeline:1.8.0"
         //compile ":handlebars-asset-pipeline:1.3.0.3"
+
+	compile ':heroku:1.0.1'
+	compile ':cloud-support:1.0.11'
     }
 }
