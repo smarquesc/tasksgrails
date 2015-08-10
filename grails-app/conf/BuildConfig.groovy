e1b1e887ea68443d7d1e84ffc8959edc7dbe9a98
Sandro Marques <sandro.marques.comp@gmail.com>
2015-08-10 00:45:28 -0300

    Teste

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,6 +51,7 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.29'
         // runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0.2-grails-2.4"
+	runtime 'postgresql:postgresql:8.4-702.jdbc3'
     }
 
     plugins {
@@ -74,6 +75,7 @@ grails.project.dependency.resolution = {
         //compile ":handlebars-asset-pipeline:1.3.0.3"
 
 	compile ':heroku:1.0.1'
-	compile ':cloud-support:1.0.11'
+	compile ':cloud-support:1.0.8'
+
     }
 }
